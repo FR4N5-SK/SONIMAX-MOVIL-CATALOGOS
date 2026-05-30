8c73c08b295995373701168d7b1ee5fbef782e89
FR4N5-SK <147278921+FR4N5-SK@users.noreply.github.com>
2026-05-30 09:06:41 -0400

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,7 +1,7 @@
 // Configuración de Supabase
-const SUPABASE_URL = "https://ebkmhvrffajaodsrmgfd.supabase.co"
+const SUPABASE_URL = "https://tuqwzrsgczhgmfnfmryw.supabase.co"
 const SUPABASE_ANON_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVia21odnJmZmFqYW9kc3JtZ2ZkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjAyNzcxMzAsImV4cCI6MjA3NTg1MzEzMH0.bCkXUogywYWQjDAjDZfKh-0QZ-0w_jKE93KNI-Fj3nU"
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR1cXd6cnNnY3poZ21mbmZtcnl3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAxMTc4NTgsImV4cCI6MjA5NTY5Mzg1OH0.-mMR7gaq_TA_PvuZKSP4o_N2sCVaP0N7ihV2Bs94na0"
 
 // Validar credenciales
 if (SUPABASE_URL === "TU_SUPABASE_URL_AQUI" || SUPABASE_ANON_KEY === "TU_SUPABASE_ANON_KEY_AQUI") {
