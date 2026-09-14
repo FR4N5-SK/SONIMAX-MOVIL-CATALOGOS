e3e33351336a9354c6f5e8bbbcc06b1811640b6d
FR4N5-SK <147278921+FR4N5-SK@users.noreply.github.com>
2026-09-14 09:46:51 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // SONIMAX MÓVIL - Service Worker con Soporte Offline Completo
 // ============================================================
 
-const CACHE_VERSION = "v10"
+const CACHE_VERSION = "v11"
 const APP_CACHE = "sonimax-app-" + CACHE_VERSION
 const IMAGE_CACHE = "sonimax-images-permanent"
 const API_CACHE = "sonimax-api-" + CACHE_VERSION
